d44a2900b57f019a8bc8fbb51d284be1e5ff07cf
Alex <alexgithinji44@gmail.com>
2024-05-26 19:46:56 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+  let compScore = 0;
+  let playerScore = 0;
+
+  function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+
+  function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return "Draw";
+    }
+
+    if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      playerScore++;
+      return 'you win';
+    } else {
+      compScore++;
+      return 'computer wins';
+    }
+  }
+
+  function playGame(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    document.getElementById('game-result').innerHTML = `  ${result}`;
+    document.getElementById('player-choice-emoji').textContent = getEmoji(playerChoice);
+    document.getElementById('comp-choice-emoji').textContent = getEmoji(computerChoice);
+    updateScoreboard();
+  }
+
+  function getEmoji(choice) {
+    switch (choice) {
+      case 'rock':
+        return '✊';
+      case 'paper':
+        return '✋';
+      case 'scissors':
+        return '✌️';
+      default:
+        return '';
+    }
+  }
+
+  function updateScoreboard() {
+    document.getElementById('score-board').textContent = `Computer ${compScore} - ${playerScore} Player`;
+  }
+
+  document.getElementById('rock-choice').addEventListener('click', function () {
+    playGame('rock');
+  });
+
+  document.getElementById('paper-choice').addEventListener('click', function () {
+    playGame('paper');
+  });
+
+  document.getElementById('scissors-choice').addEventListener('click', function () {
+    playGame('scissors');
+  });
+});
